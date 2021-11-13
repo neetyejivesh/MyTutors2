c3a33373ff75071de591cf3880a6f68dadf8b29e
Mohammed Asif Shehzaad Saydan <shehzaad786@live.com>
2021-11-13 16:09:58 +0400

    delete option

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :courses do
-    resources :bookings, only: [:create, :show, :destroy]
+    resources :bookings, only: [:create, :show]
   end
-  resources :bookings, only: :index
+  resources :bookings, only: [:index, :destroy]
 
 end
